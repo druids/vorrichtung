f4217271436297851469902511862eb706f44f9d
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2016-01-31 15:36:43 +0100

    Added tests for dom

--- test/vorrichtung/runner.cljs
+++ test/vorrichtung/runner.cljs
@@ -1,7 +1,9 @@
 (ns vorrichtung.runner
     (:require [doo.runner :refer-macros [doo-tests]]
               [vorrichtung.arg-validation-test]
+              [vorrichtung.dom-test]
               [vorrichtung.utils-test]))
 
 (doo-tests 'vorrichtung.arg-validation-test
+           'vorrichtung.dom-test
            'vorrichtung.utils-test)
