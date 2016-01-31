aa4c8bad5a80815ee9fcbf732e775a1d7c0cb380
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2016-01-31 15:24:48 +0100

    Added tests for utils

--- test/vorrichtung/runner.cljs
+++ test/vorrichtung/runner.cljs
@@ -1,5 +1,7 @@
 (ns vorrichtung.runner
     (:require [doo.runner :refer-macros [doo-tests]]
-              [vorrichtung.arg-validation-test]))
+              [vorrichtung.arg-validation-test]
+              [vorrichtung.utils-test]))
 
-(doo-tests 'vorrichtung.arg-validation-test)
+(doo-tests 'vorrichtung.arg-validation-test
+           'vorrichtung.utils-test)
