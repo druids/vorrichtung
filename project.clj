2e5174dad9b7481d2ba0174b5d6aa5a1228b678d
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2016-10-21 15:50:08 +0200

    0.9.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vorrichtung "0.8.5"
+(defproject vorrichtung "0.9.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.36"]
                  [binaryage/devtools "0.8.2"]
